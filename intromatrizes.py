12f9370e3a45c2718d898c2d9aa6c23371fe6411
Felipe Ricardo <85915186+felipricardo@users.noreply.github.com>
2023-04-27 20:08:41 -0300

    Create intromatrizes.py

---
+++ intromatrizes.py
@@ -0,0 +1,17 @@
+#matriz = [
+
+lista = [0] * 3
+#matriz = [[0,0,0],[0,0,0],[0,0,0]]
+matriz = [[0] * 3 for i in range(3)]
+matriz[0][0] = 100
+matriz[2][2] = 200
+matriz[0][2] = 50
+print(matriz)
+for linha in range(3):
+    for coluna in range(3):
+        print(matriz[linha][coluna], end="")
+    print()
+for coluna in range(3):
+    for linha in range(3):
+        print(matriz[linha][coluna], end="")
+    print()
